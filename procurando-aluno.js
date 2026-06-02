c21f5212207be465c2a2e30c213722faedded229
Pietroschoin <pietro.schon@escola.pr.gov.br>
2026-06-02 13:33:49 +0000

    refatoração

---
+++ procurando-aluno.js
@@ -0,0 +1,18 @@
+const alunos = ["João", "Juliana", "Ana", "Caio"];
+
+const medias = [10, 8, 7.5, 9];
+
+const listaDeAlunisEMedias = [alunos, medias];
+
+function exibeNomeENota(aluno) {
+    if(listaDeAlunosEMedias[0].includes(aluno)){
+        const [aluno, medias] = listaDeAlunosEMedias;
+
+        const indice = aluno.index0f(aluno);
+
+        const mediaDoAluno = medias[indice];
+        console.log(`${aluno} tem a média ${mediaDoAluno}.`)
+    }else{
+        console.log(Àluno não cadastrado!`);
+    }
+}
