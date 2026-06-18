d0ea4a4f4a9cc85399b87c0331dd3b5ba4864063
Pietroschoin <pietro.schon@escola.pr.gov.br>
2026-06-18 15:02:04 +0000

    forEach

---
+++ media-forEach.js
@@ -0,0 +1,10 @@
+const notas = [10,6.5, 8, 7.5];
+
+let somaDeNotas = 0;
+notas.forEach(function({
+    console.log('oi');
+}))
+
+const media = somaDasNotas /  notas.length;
+
+console.log ('A media das notas é $(media).');
