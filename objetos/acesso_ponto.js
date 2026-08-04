4fcdf41732fda42c3b0d0f75a7f370841d76fb65
Pietroschoin <pietro.schon@escola.pr.gov.br>
2026-08-04 15:06:47 +0000

    acessoponto

---
+++ objetos/acesso_ponto.js
@@ -0,0 +1,9 @@
+const cliente = {
+    nome: "Pietro"
+    idade: 16,
+    cpf: "14649005906"
+    email: "pietro@dominio.com" ,
+};
+
+console.log(`O nome do cliente é ${cliente.nome} e essa pessoa tem $ {cliente.idade} anos.`);
+console.log(cliente.nome);
---
+++ objetos/objeto.js
@@ -0,0 +1,10 @@
+const listaCPF = [111111, 22222, 333333,];
+
+const informacoesPessoa = ["nome" , "Pietro" , "idade" , 32, "CPF", "14649005906"];
+
+console.log(informacoesPessoa[1]);
+
+const objetoPessoa = {
+    idade= 16,
+    nome: "Pietro"
+};
